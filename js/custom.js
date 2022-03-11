17dabc66e7e9a653a90ec5b42bc3c871731952fd
Marc-Andre <Marc-Andre.Methot@cnrc-nrc.gc.ca>
2022-03-10 21:38:27 -0500

    v.1.3 - Added second nav and new section mockup

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,38 @@
 $('.sub-menu ul').hide();
 
 $(".sub-menu a").click(function () {
-	$(this).parent(".sub-menu").children("ul").slideToggle("100");
- $(this).find(".right").toggleClass("fa-caret-up fa-caret-down");
+  $(this).parent(".sub-menu").children("ul").slideToggle("100");
+  $(this).find(".right").toggleClass("fa-caret-up fa-caret-down");
+
+  // return false prevents page from reloading when clicking empty link
+  var href = $(this).attr('href');
+  if (href == "#"){
+    return false
+  }
 });
 
 $("#navHamburger").click(function () {
   $('#wb-sec').slideToggle("100");
   $(this).children('span').toggleClass('fa-solid fa-xmark')
+  // return false prevents page from reloading
+  return false
 });
 
+
+//Expand collapse the nav in mobile/tablet views
 let hamNav = document.querySelector("#navHamburger")
 
 hamNav.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
-   event.preventDefault();
+    event.preventDefault();
     document.getElementById("myBtn").click();
   }
 });
 
+// Check for any active classes
+var activeNav = document.getElementsByClassName("active")
+
+if (activeNav.length > 0) {
+  $(activeNav[0]).parents(".sub-menu").children("ul").slideToggle("100");
+  $(activeNav[0]).parents(".sub-menu").find(".right").toggleClass("fa-caret-up fa-caret-down");
+}
